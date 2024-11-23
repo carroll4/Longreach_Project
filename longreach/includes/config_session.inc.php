7141dcead2ab8612b322b5bf3e8e87677dd15f34
carroll4 <93520577+carroll4@users.noreply.github.com>
2024-11-23 19:31:26 +0000

    update session
    session ID set before starting session

--- longreach/includes/config_session.inc.php
+++ longreach/includes/config_session.inc.php
@@ -3,6 +3,7 @@
 ini_set('session.use_only_cookies', 1);
 ini_set('session.use_strict_mode', 1);
 
+// Set session cookie parameters
 session_set_cookie_params([
     'lifetime' => 1800,
     'domain' => 'localhost',
@@ -10,11 +11,9 @@ session_set_cookie_params([
     'secure' => true,
     'httponly' => true
 ]);
-//start session
-session_start();
 
-//regenerate session every 30mins
-if(isset($_SESSION["user_id"])){
+// Regenerate session ID before session start
+if (isset($_SESSION["user_id"])) {
     if (isset($_SESSION["last_regeneration"])) {
         regenerate_session_id_loggedin();
     } else {
@@ -33,19 +32,33 @@ if(isset($_SESSION["user_id"])){
         }
     }
 }
-//regenerate session id with user id(only works if user is logged in)
+
+// Function to regenerate session ID when logged in
 function regenerate_session_id_loggedin() {
+    // Regenerate the session ID before starting the session
     session_regenerate_id(true);
 
+    // Set a custom session ID using user_id (if needed)
     $userId = $_SESSION["user_id"];
     $newSessionId = session_create_id();
     $sessionId = $newSessionId . "_" . $userId;
     session_id($sessionId);
 
+    // Start the session
+    session_start();
+
+    // Save the last regeneration time
     $_SESSION["last_regeneration"] = time();
 }
-//regenerate session id
+
+// Function to regenerate session ID for non-logged-in users
 function regenerate_session_id() {
+    // Regenerate the session ID
     session_regenerate_id(true);
+
+    // Start the session
+    session_start();
+
+    // Save the last regeneration time
     $_SESSION["last_regeneration"] = time();
 }
