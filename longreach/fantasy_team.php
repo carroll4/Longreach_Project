d13b500be36a6c95683b3be86f9982895f8b4bd2
carroll4 <93520577+carroll4@users.noreply.github.com>
2024-12-06 14:10:10 +0000

    footer_p id added

--- longreach/fantasy_team.php
+++ longreach/fantasy_team.php
@@ -245,7 +245,7 @@ require_once 'includes/login_view.inc.php';
                 <a href="https://www.instagram.com/longreach_afc/?hl=en-gb" target="_blank"><img src="Images/insta.jpg" style="float: right; width: 8vw; height: 8vw; padding: 2px;"></a>
                 <a href="https://twitter.com/longreachutr" target="_blank"><img src="Images/twitter.png" style="float: right; width: 8vw; height: 8vw; padding: 2px;"></a>
                 <br>
-                <p style="float: left;"><b>Copy Right | Privacy Notice | Terms of Use</b></p>
+                <p id="footer_p"><b>Copy Right | Privacy Notice | Terms of Use</b></p>
             </div>
 
             <div class="left">
