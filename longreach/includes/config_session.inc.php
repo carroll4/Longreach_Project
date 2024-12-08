25b751c9117045ec96d9ee0145fa1f05e9d0a8b2
carroll4 <lewis2.carroll@live.uwe.ac.uk>
2024-12-08 15:50:22 +0000

    Update config_session.inc.php

--- longreach/includes/config_session.inc.php
+++ longreach/includes/config_session.inc.php
@@ -11,41 +11,38 @@ session_set_cookie_params([
     'secure' => true,
     'httponly' => true
 ]);
-// start session
+
+// Start session
 session_start();
 
-// regenerate session every 30mins
-if(isset($_SESSION["user_id"])){
-    if (isset($_SESSION["last_regeneration"])) {
+// Regenerate session every 30 minutes
+if (isset($_SESSION["user_id"])) {
+    $interval = 60 * 30;
+
+    if (!isset($_SESSION["last_regeneration"]) || time() - $_SESSION["last_regeneration"] >= $interval) {
         regenerate_session_id_loggedin();
-    } else {
-        $interval = 60 * 30;
-        if (time() - $_SESSION["last_regeneration"] >= $interval) {
-            regenerate_session_id_loggedin();
-        }
     }
 } else {
-    if (!isset($_SESSION["last_regeneration"])) {
+    $interval = 60 * 30;
+
+    if (!isset($_SESSION["last_regeneration"]) || time() - $_SESSION["last_regeneration"] >= $interval) {
         regenerate_session_id();
-    } else {
-        $interval = 60 * 30;
-        if (time() - $_SESSION["last_regeneration"] >= $interval) {
-            regenerate_session_id();
-        }
     }
 }
-// regenerate session id with user id(only works if user is logged in)
+
+// Regenerate session ID with user ID (only works if user is logged in)
 function regenerate_session_id_loggedin() {
     session_regenerate_id(true);
 
+    // Generate a new session ID with the user ID
     $userId = $_SESSION["user_id"];
-    $newSessionId = session_create_id();
-    $sessionId = $newSessionId . "_" . $userId;
-    session_id($sessionId);
+    $newSessionId = session_create_id($userId . "_"); // session_create_id() generates a new session ID safely.
 
+    // Update the session with the new ID
     $_SESSION["last_regeneration"] = time();
 }
-// regenerate session id
+
+// Regenerate session ID
 function regenerate_session_id() {
     session_regenerate_id(true);
     $_SESSION["last_regeneration"] = time();
